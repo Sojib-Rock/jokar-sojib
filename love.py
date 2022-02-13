ba357b5c58f619d54dcfd2181871c217a15b4d32
Sojib-Rock <99610499+Sojib-Rock@users.noreply.github.com>
2022-02-13 23:51:04 +0600

    Update love.py

--- love.py
+++ love.py
@@ -37,12 +37,12 @@ def jalan(z):
 		time.sleep(00000.1)
 ##### LOGO #####
 logo = """
-   Asif Javed
+   JOKAR-SOJIB
    ▄︻̷̿┻̿═━一
-   THE ANONYMOUS lovehacker TRICKER 
-   THE LEGEND ℒℴνℯ
-   THE GAME CHANGER ℒℴνℯ
-   pakistani hackers
+   THE MD.NIROB HASAN
+   THE Jokar...Bd..Hacked
+   THE Mymensingh....
+   Bangladesh.....
    ℒℴνℯ ▄︻̷̿┻̿═━一
 \033[1;91m=======================================
 \033[1;96mAuthor  \033[1;93m: \033[1;92mlove
@@ -72,8 +72,8 @@ print  """\033[1;91m=======================================
 \033[1;96mpage \033[1;93m: \033[1;92mhttps://www.facebook.com/lovehacker
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
-CorrectUsername = "lovehacker"
-CorrectPassword = "03094161457"
+CorrectUsername = jokarsojib66
+CorrectPassword = magibazsojib67
 loop = 'true'
 while (loop == 'true'):
     username = raw_input("\033[1;96m \x1b[1;93mUsername Of Tool \x1b[1;96m>>>> ")
